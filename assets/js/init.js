47cf2e9b0db18fee5b87205d3c7783a36b4a7315
ellie <ellie.parobek@gmail.com>
2019-03-03 00:51:07 -0500

    first

---
+++ assets/js/init.js
@@ -0,0 +1,16 @@
+// initialize necessary materialize components
+(function($){
+    $(function(){
+        // side bar navigation
+        $('.sidenav').sidenav();
+        
+        // modal
+        $('.modal').modal();
+        
+        // card tab
+        // $('.tabs').tabs();
+        
+        // dropdown
+        // $('.dropdown-trigger').dropdown();
+    });
+})(jQuery);
